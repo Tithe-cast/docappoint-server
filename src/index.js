d05b328700f6173e58a003f164807da5450adcf2
Syeda_Sima <simaakter089129@gmail.com>
2026-05-21 17:20:48 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     process.env.CLIENT_URL || 'http://localhost:5173',
-   'https://docappoint-client-virid.vercel.app',
+    'https://docappoint-client-virid.vercel.app',
 
   ],
   credentials: true,
